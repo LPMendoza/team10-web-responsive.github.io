b66525e2fc618eebd5f728e783e83f569c71c20e
Pedro Mendoza <lpedromendoza333@gmail.com>
2020-12-03 18:14:59 -0600

    Url shorten finished

--- js/shorten.js
+++ js/shorten.js
@@ -17,20 +17,46 @@ document.getElementById("btnShorten").onclick = function (e) {
    fetch(`${API_URL}?url=${txtShorten.value.trim()}`)
    .then(response => response.json())
    .then(link =>{ 
-      urls.push(link.result);
+      urls.push({
+         "originalUrl": txtShorten.value,
+         "urlShorten": link.result.short_link
+      });
 
       var contLinksShortened = document.getElementById("contLinksShortened");
       contLinksShortened.innerHTML = "";
 
-      urls.forEach(url => {
-      contLinksShortened.innerHTML += `<div class="contShorten bg-white">
-        <h3 class="linkToShorten">${ txtShorten.value}</h3>
-        <div class="contControlsLink">
-          <input type="text" class="linkShortened" value="${url.short_link}">
-          <button class="btn btn-cyan text-white btnCopy">Copy</button>
-        </div>
-      </div>`
+      urls.forEach((url, index) => {
+         contLinksShortened.innerHTML += 
+            `<div class="contShorten bg-white">
+               <div class="contOriginalLink">
+                  <p class="linkToShorten">${url.originalUrl}</p>
+               </div>
+               <div class="contControlsLink">
+                  <label id="txtShortened-${index}" class="linkShortened">${url.urlShorten}</label>
+                  <button id="btnCopy-${index}" class="btn btn-cyan text-white btnCopy">Copy</button>
+               </div>
+            </div>`;
       });
+      
+      var btns = Array.from(document.getElementsByClassName("btnCopy"));
+
+      btns.forEach(el => {
+         el.onclick = function (e) {
+            
+            var btnCopied = Array.from(document.getElementsByClassName("bgCopied"));
+            if(btnCopied.length != 0) {
+               btnCopied[0].classList.remove("bgCopied");
+               btnCopied[0].textContent = "Copy";
+            }
+
+            document.getElementById("urlCopy").value = document.getElementById("txtShortened-" + this.id.split("-")[1]).textContent;
+            document.getElementById("urlCopy").focus();
+            document.getElementById("urlCopy").select();
+            document.execCommand("copy");
+            this.classList.add("bgCopied");
+            this.textContent = "Copied!";
+         }
+      })
       
       txtShorten.value = "";
 
